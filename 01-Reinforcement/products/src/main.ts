b1d3bc42708c56d4ffd9a564ce794d8cb788e2a3
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-08-06 15:11:07 -0500

    SS - Environtment variables, Joi for schemas validations

--- 01-Reinforcement/products/src/main.ts
+++ 01-Reinforcement/products/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = envs.port ?? 3000;
+  await app.listen(port);
+  console.log(`Server running on port ${port}`)
 }
 bootstrap();
